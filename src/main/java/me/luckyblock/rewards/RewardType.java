606249297603a2b42c4789cf46ec6ddff7805e97
youness <younesss9888@gmail.com>
2025-04-17 17:37:28 +0200

    Update RewardType.java

--- src/main/java/me/luckyblock/rewards/RewardType.java
+++ src/main/java/me/luckyblock/rewards/RewardType.java
@@ -1,9 +1,10 @@
-
 package me.luckyblock.rewards;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import me.luckyblock.Main;
 import me.luckyblock.hooks.VaultHook;
 
@@ -12,12 +13,14 @@ public enum RewardType {
         @Override
         public void giveReward(Player player) {
             player.getInventory().addItem(new ItemStack(Material.DIAMOND, 5));
+            player.sendMessage("§a§lHai trovato §b5 diamanti§a!");
         }
     },
     EMERALD {
         @Override
         public void giveReward(Player player) {
             player.getInventory().addItem(new ItemStack(Material.EMERALD, 3));
+            player.sendMessage("§a§lHai trovato §23 smeraldi§a!");
         }
     },
     MONEY {
@@ -28,6 +31,27 @@ public enum RewardType {
                 vaultHook.getEconomy().depositPlayer(player, 1000);
                 player.sendMessage("§a§lHai vinto §e1000 monete§a!");
             }
+        }
+    },
+    NETHERITE {
+        @Override
+        public void giveReward(Player player) {
+            player.getInventory().addItem(new ItemStack(Material.NETHERITE_INGOT, 1));
+            player.sendMessage("§a§lHai trovato §8netherite§a!");
+        }
+    },
+    SPEED_BOOST {
+        @Override
+        public void giveReward(Player player) {
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 1));
+            player.sendMessage("§a§lHai ottenuto §bvelocità§a!");
+        }
+    },
+    GOLDEN_APPLE {
+        @Override
+        public void giveReward(Player player) {
+            player.getInventory().addItem(new ItemStack(Material.GOLDEN_APPLE, 3));
+            player.sendMessage("§a§lHai trovato §63 mele d'oro§a!");
         }
     };
 
