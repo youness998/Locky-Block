e9a90adea850225712972d3a42840416afd15398
youness <younesss9888@gmail.com>
2025-06-06 17:01:42 +0200

    Update BlockChecker.java

--- src/main/java/me/luckyblock/checker/BlockChecker.java
+++ src/main/java/me/luckyblock/checker/BlockChecker.java
@@ -10,7 +10,7 @@ public class BlockChecker {
     }
     
     public static boolean isInProtectedRegion(Block block) {
-        // Implementazione per controllo regioni protette
+        
         return false;
     }
 }
